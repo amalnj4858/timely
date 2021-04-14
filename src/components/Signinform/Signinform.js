8b772560e589c4f74a6db505c3a3d0bc87ab35f7
Amal Najeena Jaleel <amalnj4858@gmail.com>
2021-04-15 00:29:17 +0530

    integrated firebase

--- src/components/Signinform/Signinform.js
+++ src/components/Signinform/Signinform.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import './Signinform.css'
+import './Signinform.css';
+import {signInWithPopUp} from '../../firebase/firebase.config';
 
 const Signinform = ()=>{
+    const popUp  = ()=>{
+        signInWithPopUp();
+    }
     return(
         <div className='signinform'>
             <h2 className='signin'>SIGN IN</h2>
             <span className='formInfo'>
                 Please sign in using your bitsmail to continue
             </span>
-            <button className='signinButton'>SIGN IN WITH GOOGLE</button>
+            <button className='signinButton' onClick={popUp}>SIGN IN WITH GOOGLE</button>
         </div>
     )
 }
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+
+import "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBgpKXOnT0vGohknyN-sjLzWV62krIbdwc",
+    authDomain: "timely-57359.firebaseapp.com",
+    projectId: "timely-57359",
+    storageBucket: "timely-57359.appspot.com",
+    messagingSenderId: "423253232400",
+    appId: "1:423253232400:web:48d8d61eda2737b8893395",
+    measurementId: "G-YCP4R2CWPE"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+  var provider = new firebase.auth.GoogleAuthProvider();
+
+  export const signInWithPopUp = ()=> firebase.auth().signInWithPopup(provider);
