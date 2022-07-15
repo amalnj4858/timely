ae78197535e92f683e8227f2949acefec6a74f3c
amalnj4858 <amalnj4858@gmail.com>
2022-07-15 15:06:54 +0530

    last change

--- src/App.js
+++ src/App.js
@@ -1,61 +1,70 @@
-import React,{useEffect} from 'react';
-import './App.css';
-import Header from './components/Header/Header.js';
-import Signinpage from './pages/Signinpage/Signinpage.js';
-import {connect} from 'react-redux';
-import firebase from './firebase/firebase.config.js';
-import {setUser} from './redux/User/User-actions.js';
-import {Route,Redirect,Switch} from 'react-router-dom';
-import Notespage from './pages/Notespage/Notespage';
-import Homepage from './pages/Homepage/Homepage';
-import Reminderspage from './pages/Reminderspage/Reminderspage';
-import WeeklyPlannerpage from './pages/WeeklyPlannerpage/WeeklyPlannerpage';
+import React, { useEffect } from "react";
+import "./App.css";
+import Header from "./components/Header/Header.js";
+import Signinpage from "./pages/Signinpage/Signinpage.js";
+import { connect } from "react-redux";
+import firebase from "./firebase/firebase.config.js";
+import { setUser } from "./redux/User/User-actions.js";
+import { Route, Redirect, Switch } from "react-router-dom";
+import Notespage from "./pages/Notespage/Notespage";
+import Homepage from "./pages/Homepage/Homepage";
+import Reminderspage from "./pages/Reminderspage/Reminderspage";
+import WeeklyPlannerpage from "./pages/WeeklyPlannerpage/WeeklyPlannerpage";
 
-
-const App =({setCurrentUser,currentUser})=> {
-  
-  useEffect(()=>{
-    if(!currentUser){
-      const unsubscribeAuth = firebase.auth().onAuthStateChanged(user=>{           //signs in user only if its a bits hyderabad email id.
-        if(user){
-          if(user.email.split('@')[1]==='hyderabad.bits-pilani.ac.in'){
-            const {email,displayName,uid} = user;
+const App = ({ setCurrentUser, currentUser }) => {
+  useEffect(() => {
+    if (!currentUser) {
+      const unsubscribeAuth = firebase.auth().onAuthStateChanged((user) => {
+        //signs in user only if its a bits hyderabad email id.
+        if (user) {
+          if (true) {
+            const { email, displayName, uid } = user;
             const createdAt = new Date();
             setCurrentUser({
-              displayName,uid,email,createdAt
-            })
-          }
-          else
-            alert('Please use bitsmail only')
+              displayName,
+              uid,
+              email,
+              createdAt,
+            });
+          } else alert("Please use bitsmail only");
         }
-      }
-    )
-    return ()=>{
-        unsubscribeAuth();   //using the hook like componentWillUnmount. Done to avoid any memory leaks as the user signs out. 
+      });
+      return () => {
+        unsubscribeAuth(); //using the hook like componentWillUnmount. Done to avoid any memory leaks as the user signs out.
+      };
     }
-    }
-})
+  });
 
   return (
     <div className="App">
-      <Header/>
-        <Switch>
-          <Route exact path = '/' render = {()=>currentUser ? <Redirect to = '/home' />: <Signinpage/> } />
-          <Route exact path =  '/home' component = {Homepage} />
-          <Route exact path =  '/home/notes' component = {Notespage} />
-          <Route exact path = '/home/reminders' component = {Reminderspage} />
-          <Route exact path =  '/home/weekly-planner' component = {WeeklyPlannerpage} />
-        </Switch>
+      <Header />
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() =>
+            currentUser ? <Redirect to="/home" /> : <Signinpage />
+          }
+        />
+        <Route exact path="/home" component={Homepage} />
+        <Route exact path="/home/notes" component={Notespage} />
+        <Route exact path="/home/reminders" component={Reminderspage} />
+        <Route
+          exact
+          path="/home/weekly-planner"
+          component={WeeklyPlannerpage}
+        />
+      </Switch>
     </div>
   );
-}
+};
 
-const mapDispatchToProps = (dispatch)=>({
-  setCurrentUser : user=>dispatch(setUser(user))
-})
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setUser(user)),
+});
 
-const mapStateToProps = (state)=>({
-  currentUser : state.user.currentUser
-})
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser,
+});
 
-export default connect(mapStateToProps,mapDispatchToProps)(App);  //Higher order component having access to the current user from the store.
+export default connect(mapStateToProps, mapDispatchToProps)(App); //Higher order component having access to the current user from the store.
